c75c4763beafe48897794d9964a5fe1c5ec09adb
kherma <kamil.herma01@gmail.com>
2021-03-29 19:49:19 +0200

    Setup socket.io on server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // ================
 
 const express = require("express");
+const socket = require("socket.io");
+const { tasks } = require("./db");
 
 // ================
 // Server Setup
@@ -13,6 +15,32 @@ const app = express();
 
 const server = app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on port: ${port}`);
+});
+const io = socket(server);
+
+// ================
+// Server Events
+// ================
+
+io.on("connect", (socket) => {
+  // Emit tasks array to connected socket
+  socket.emit("updateData", tasks);
+
+  // Listener of new task
+  socket.on("addTask", (task) => {
+    tasks.push(task);
+
+    // Emit new task to other users
+    socket.broadcast.emit("addTask", task);
+  });
+
+  // Listener of remove task
+  socket.on("removeTask", (index) => {
+    tasks.splice(index, 1);
+
+    // Emit remove task to other users
+    socket.broadcast.emit("removeTask", index);
+  });
 });
 
 // ================
